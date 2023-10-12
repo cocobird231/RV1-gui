422d68a9c661101da40fcbf7fe0726ee1fcd1b74
gluttonyOwO <41547584+gluttonyOwO@users.noreply.github.com>
2023-10-12 15:10:07 +0800

    add sleep remove

--- ws/src/ros_qt_sen/src/qos_device_dialog/qosdevicedialog.cpp
+++ ws/src/ros_qt_sen/src/qos_device_dialog/qosdevicedialog.cpp
@@ -236,6 +236,7 @@ void QosDeviceDialog::remove_for_message_type( QString message_type){
         vehicle_interfaces::srv::QosReg::Request req;
         req.topic_name = topic.toStdString();
         req.remove_profile = true;
+        sleep(1);
         bool qos_requst_success = control->requestQosReg(std::make_shared<vehicle_interfaces::srv::QosReg::Request>(req));
         if (!qos_requst_success)
         {
